0dac8aead03768b477cfcbed4f1b64e728f9bf89
Jack Lin <446026340@qq.com>
2024-11-03 11:38:21 +0800

    feat: make the sidebar trigger button bigger

--- src/app/components/sidebar-collapse-button.tsx
+++ src/app/components/sidebar-collapse-button.tsx
@@ -9,7 +9,7 @@ export default function SidebarCollapseButton() {
             className="text-gray-300 hover:text-white"
             onClick={toggleSidebar}
         >
-            <PanelLeft className="w-[15px] h-[15px]"/>
+            <PanelLeft className="w-[18px] h-[18px]"/>
         </button>
     )
 }
