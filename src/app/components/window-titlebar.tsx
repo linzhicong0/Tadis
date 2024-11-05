e2f8afc439e618017653e588f3ef58f51baf6de1
Jack Lin <446026340@qq.com>
2024-11-05 22:56:47 +0800

    feat: add a mode button to the window bar to change the dark or light theme

--- src/app/components/window-titlebar.tsx
+++ src/app/components/window-titlebar.tsx
@@ -4,12 +4,22 @@ import MinButton from './min-button';
 import MaxButton from './max-button';
 import SidebarCollapseButton from './sidebar-collapse-button';
 import { Window } from '@tauri-apps/api/window'
+import { Moon, Sun } from 'lucide-react';
+import { useState } from 'react';
 
 export default function WindowTitleBar() {
 
     const handleClose = () => Window.getCurrent().close();
     const handleMinimize = () => Window.getCurrent().minimize();
     const handleMaximize = () => Window.getCurrent().toggleMaximize();
+
+    const [theme, setTheme] = useState('light');
+
+    const toggleTheme = () => {
+        const newTheme = theme === 'light' ? 'dark' : 'light';
+        setTheme(newTheme);
+        document.documentElement.classList.toggle('dark');
+    };
 
     return (
         <div className="flex flex-row">
@@ -21,8 +31,18 @@ export default function WindowTitleBar() {
                     <MinButton handleMinimize={handleMinimize} />
                     <MaxButton handleMaximize={handleMaximize} />
                 </div>
-                <div className="flex items-center align-middle py-3">
+                <div className="flex items-center align-middle py-3 gap-2">
                     <SidebarCollapseButton />
+                    <button
+                        onClick={toggleTheme}
+                        className="text-gray-300 hover:text-white"
+                    >
+                        {theme === 'light' ? (
+                            <Moon className='w-[18px] h-[18px]'/>
+                        ) : (
+                            <Sun className='w-[18px] h-[18px]'/>
+                        )}
+                    </button>
                 </div>
             </div>
         </div>
