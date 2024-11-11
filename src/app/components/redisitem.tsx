aa3ff184a41cbf2d6c9e4c0c853a34b00867d9b4
Jack Lin <446026340@qq.com>
2024-11-11 21:48:35 +0800

    feat: add delete button to the redis item

--- src/app/components/redisitem.tsx
+++ src/app/components/redisitem.tsx
@@ -1,3 +1,4 @@
+import { Trash2 } from 'lucide-react';
 import React from 'react';
 
 type RedisItemType = 'string' | 'set' | 'hash' | 'list' | 'stream' | 'zset';
@@ -5,9 +6,10 @@ type RedisItemType = 'string' | 'set' | 'hash' | 'list' | 'stream' | 'zset';
 interface RedisItemProps {
   type: RedisItemType;
   name: string;
+  onDelete?: (name: string) => void;
 }
 
-const RedisItem: React.FC<RedisItemProps> = ({ type, name }) => {
+const RedisItem: React.FC<RedisItemProps> = ({ type, name, onDelete }) => {
   const getTypeColor = (type: RedisItemType) => {
     const colors = {
       string: 'bg-green-500',
@@ -21,13 +23,26 @@ const RedisItem: React.FC<RedisItemProps> = ({ type, name }) => {
   };
 
   return (
-    <div className="flex items-center gap-2">
-      <span className={`${getTypeColor(type)} text-white rounded-sm font-semibold uppercase text-[10px] w-14 h-5 flex items-center justify-center`}>
-        {type}
-      </span>
-      <span className="text-gray-200 text-sm">
-        {name}
-      </span>
+    <div className="flex items-center justify-between gap-2 hover:bg-gray-800 rounded-md p-1 hover:cursor-pointer">
+      <div className="flex items-center gap-2">
+        <span className={`${getTypeColor(type)} text-white rounded-sm font-semibold uppercase text-[10px] w-14 h-5 flex items-center justify-center`}>
+          {type}
+        </span>
+        <span className="text-gray-200 text-sm">
+          {name}
+        </span>
+      </div>
+      {onDelete && (
+        <button
+          onClick={(e) => {
+            e.stopPropagation();
+            onDelete(name);
+          }}
+          className="text-gray-400 hover:text-red-500 p-1"
+        >
+          <Trash2 strokeWidth={1.5} className="w-4 h-4" />
+        </button>
+      )}
     </div>
   );
 };
