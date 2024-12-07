c9c31e839eef81c41bea4c53398299b42f06a265
Jack Lin <446026340@qq.com>
2024-12-07 10:37:27 +0800

    feat: add refresh function and update the toast color

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -14,8 +14,14 @@ const Toaster = ({ ...props }: ToasterProps) => {
       className="toaster group"
       toastOptions={{
         classNames: {
-          toast:
-            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+          toast: [
+            "group toast group-[.toaster]:border-gray-700 group-[.toaster]:shadow-lg",
+            "group-[.toaster]:bg-background group-[.toaster]:text-foreground",
+            "data-[type=success]:group-[.toaster]:bg-green-500 data-[type=success]:group-[.toaster]:text-white",
+            "data-[type=info]:group-[.toaster]:bg-blue-500 data-[type=info]:group-[.toaster]:text-white",
+            "data-[type=error]:group-[.toaster]:bg-red-500 data-[type=error]:group-[.toaster]:text-white",
+            "data-[type=warning]:group-[.toaster]:bg-yellow-500 data-[type=warning]:group-[.toaster]:text-white",
+          ].join(" "),
           description: "group-[.toast]:text-muted-foreground",
           actionButton:
             "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
